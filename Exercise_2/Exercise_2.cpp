b8ae6ab2c070bc45143802ebb495c55d6955bf0f
Maciej Kaca <maciek.kaca@gmail.com>
2018-05-12 00:27:39 +0200

    +Reading inputs

--- Exercise_2/Exercise_2.cpp
+++ Exercise_2/Exercise_2.cpp
@@ -2,11 +2,37 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <sstream>
+#include <iterator>
 
+using namespace std;
 
 int main()
 {
-	test;
+	struct CaseStruct {
+		int Center;
+		vector<int> Elements;
+	};
+
+	int CaseQuantity;
+	vector <int> Eelements;
+	cin >> CaseQuantity;
+	CaseStruct *Case = new CaseStruct[CaseQuantity];
+	cin.ignore();
+	for (int _case = 0; _case < CaseQuantity; _case++)
+	{
+		string elementsStr;
+		getline(cin, elementsStr);
+		istringstream is(elementsStr);
+		vector<int> v((istream_iterator<int>(is)), istream_iterator<int>());
+		Case[_case].Elements = v;
+	}
+
+	delete[] Case;
+	_getch();
     return 0;
 }
 

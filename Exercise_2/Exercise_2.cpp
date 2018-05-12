4a46a533c998c97ac5e9fb7f7472751dc1faff4f
Maciej Kaca <maciek.kaca@gmail.com>
2018-05-12 02:17:41 +0200

    +Checking if there is an element whose sum of elements on its left is equal to the sum of elements on its right side

--- Exercise_2/Exercise_2.cpp
+++ Exercise_2/Exercise_2.cpp
@@ -12,23 +12,41 @@ using namespace std;
 
 int main()
 {
-	struct CaseStruct {
-		int Center;
-		vector<int> Elements;
-	};
 
 	int CaseQuantity;
-	vector <int> Eelements;
 	cin >> CaseQuantity;
-	CaseStruct *Case = new CaseStruct[CaseQuantity];
-	cin.ignore();
+	vector<int> *Case = new vector<int>[CaseQuantity];
 	for (int _case = 0; _case < CaseQuantity; _case++)
 	{
 		string elementsStr;
+		int IUseVectors;
+		cin >> IUseVectors;
+		cin.ignore();
 		getline(cin, elementsStr);
 		istringstream is(elementsStr);
 		vector<int> v((istream_iterator<int>(is)), istream_iterator<int>());
-		Case[_case].Elements = v;
+		v.erase(v.begin() + IUseVectors, v.end());
+		Case[_case] = v;
+	}
+
+	for (int i = 0; i < CaseQuantity; i++)
+	{
+		int Left=Case[i].at(0), Right=0;
+		for (int element = 2; element < Case[i].size(); element++)
+		{
+			Right += Case[i].at(element);
+		}
+		for (int element = 1; element < Case[i].size() && Left != Right; element++)
+		{
+			Left += Case[i].at(element);
+			Right -= Left;
+		}
+		if (Left == Right || Case[i].size()==1)
+		{
+			cout << "YES" << endl;
+		}
+		else
+			cout << "NO" << endl;
 	}
 
 	delete[] Case;
